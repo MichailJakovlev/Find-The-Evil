c382917756272222d01b25a254fd6c89ba10049e
Michail Jakovlev <you@example.commichailjakovlev4@gmail.com>
2025-08-15 21:01:28 +0400

    add card pool

---
+++ Assets/Native/Scripts/Cards/Card.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+public class Card : MonoBehaviour
+{
+    public Role _cardRole;
+    public SpriteRenderer _cardImage;
+    
+}
+
---
+++ Assets/Native/Scripts/Cards/CardPool.cs
@@ -0,0 +1,67 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class gitCardPool : MonoBehaviour
+{
+    [SerializeField] private Card cardPrefab;
+    [SerializeField] private Card[] cards;
+    
+    public int _maxCardAmount;
+    public int _cardAmount;
+    
+    private Vector3 _axis;
+    void Start()
+    {
+        cards = new Card[_maxCardAmount];
+        Create();
+    }
+
+    public void Create()
+    {
+        for (int i = 0; i < _maxCardAmount; i++)
+        {
+            cards[i] = Instantiate(cardPrefab, transform);
+            cards[i].gameObject.SetActive(false);
+        }
+
+        Get(_cardAmount);
+    }
+    
+    public void Get(int cardAmount)
+    {
+        for (int i = 0; i < cardAmount; i++)
+        {
+            cards[i].gameObject.SetActive(true);
+        }
+        PositionChanger(cardAmount);
+    }
+
+    public void PositionChanger(int value)
+    {
+        int shiftValue = (value - 1) / 2;
+        for (int i = 0; i < value; i++)
+        {
+            float _rotateValue = 360 / value * i;
+            _axis.z = _rotateValue;
+            
+            if (360 % value != 0 && i > (value - 1) / 2)
+            {
+                _rotateValue = 360 / value * shiftValue;
+                shiftValue--;
+                _axis.z = -_rotateValue;
+            }
+            
+              cards[i].gameObject.transform.rotation = new Quaternion(0,0,0,0);
+              cards[i].gameObject.transform.Rotate(_axis);
+              cards[i]._cardImage.GameObject().transform.rotation = Quaternion.identity;
+        }
+    }
+    
+    public void Realize()
+    {
+        for (int i = 0; i < _maxCardAmount; i++)
+        {
+            cards[i].gameObject.SetActive(false);
+        }
+    }
+}
